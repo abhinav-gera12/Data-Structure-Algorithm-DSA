bad113dcd421a39f8fd1fa7a9c9eec10a09f2048
Abhinav Gera <63844738+abhinav-gera12@users.noreply.github.com>
2025-03-20 17:55:34 +0000

    operators outline

---
+++ python-basics/operators.py
@@ -0,0 +1,10 @@
+# OPERATORS
+
+'''
+Outline:
+i) Introduction to operators
+ii) Arithmetic Operators - Addition, Subtraction, Multiplication, Division, Floor Division, Modulus, Exponentiation
+iii) Comparision Operators - Equal to, Not equal to, Greather than, Less than, Greater than or equal to, Less than or equal to
+iv) Logical Operators - AND, OR, NOT
+'''
+
