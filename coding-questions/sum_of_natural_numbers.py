488705b84d865966084088eb7a7d70ce5eeb153d
Abhinav Gera <63844738+abhinav-gera12@users.noreply.github.com>
2025-03-22 07:13:20 +0000

    sum of natural numbers

---
+++ coding-questions/sum_of_natural_numbers.py
@@ -0,0 +1,21 @@
+'''
+(Q)- Calcualte the sum of first N natural numbers using whlie loop as well as for loop
+'''
+
+# WHILE LOOP
+print("while loop - ")
+n = int(input("enter a number - "))
+sum = 0
+while n >= 1:
+    sum = sum + n
+    n = n - 1
+print(sum)
+
+
+# FOR LOOP
+print("for loop - ")
+n = int(input("enter a number - "))
+sum = 0
+for i in range(1, n+1):
+    sum = sum + i
+print(sum)
