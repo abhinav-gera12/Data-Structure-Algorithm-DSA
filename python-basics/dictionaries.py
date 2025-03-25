a188951a38737e585e4f40c6654b8f2561a1139b
Abhinav Gera <63844738+abhinav-gera12@users.noreply.github.com>
2025-03-25 08:27:49 +0000

    dictionary completed

--- python-basics/dictionaries.py
+++ python-basics/dictionaries.py
@@ -64,3 +64,83 @@ student['address'] = 'AB Colony'    ## insert a new key and value
 print(student)
 del student['grade']        ## delete the key-value pair from the original dictionary
 print(student)
+
+
+# DICTIONARY METHODS
+print('dictionary methods - ')
+keys = student.keys()
+print(keys)
+values = student.values()
+print(values)
+items = student.items()
+print(items)
+
+## SHALLOW COPY
+'''
+- SHALLOW COPY -> It provides the different memory allocation to the variable, so if we change one of the variable, it doesn't reflect
+                    the other variable's memory address
+'''
+student_copy = student
+print(student)
+print(student_copy)
+
+student['name'] = 'Abhinav2'
+print(student)
+print(student_copy)         ## It will also reflect the new value, so it will reflect the same values which are present in the dict
+
+student['name'] = 'Abhinav Gera'
+student_copy1 = student.copy()
+print(student)
+print(student_copy)
+print(student_copy1)
+
+student['name'] = 'Abhinav Gera 2'
+print(student)
+print(student_copy)
+print(student_copy1)
+
+
+# ITERATING OVER DICTIONARIES
+'''
+- Iterating over dictionaries -> You can use the loops to iterate over dictionaries, keys, values and items
+'''
+print('iterating over dictionaries - ')
+## iterating over keys
+student = {"name":'Abhinav', 'age':22, 'grade':'A'}
+for keys in student.keys():
+    print(keys)
+## iterating over values
+for values in student.values():
+    print(values)
+## iterating over key-value pairs
+for key, value in student.items():
+    print(f'{key} : {value}')
+
+
+# NESTED DICTIONARIES
+'''
+- Nested dictionaries -> A dictionary inside of another dictionary
+'''
+print('nested dictionary - ')
+students = {
+    "student1": {'name':'Abhinav', 'age':32},
+    'student2': {'name':'Krish', 'age':15}
+}
+print(students)
+
+# ACCESS NESTED DICTIONARIES ELEMENTS
+print(students['student2']['name'])
+
+# ITERATING OVER NESTED DICTIONARIES
+for student_id, student_info in students.items():
+    print(f'{student_id} : {student_info}')
+    for key, value in student_info.items():
+        print(f"{key} : {value}")
+
+
+# DICTIONARY COMPREHENSION
+squares = {x:x**2 for x in range(1,6)}
+print(squares)
+
+squares_even = {x:x**2 for x in range(1,10) if x%2==0}
+print(squares_even)
